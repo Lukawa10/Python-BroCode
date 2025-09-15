7f23ab864c4376bbc6d161cee9e95e40e12541b4
Lukawa10 <raweepatpro@gmail.com>
2025-09-15 18:09:15 +0700

    bro code lets go

--- main.py
+++ main.py
@@ -848,3 +848,533 @@
 #     else:       #so if user guess something that is not a digit then it will show "Invalid guess" 
 #         print("Invalid guess")        
 #         print(f"Please select a number between {lowest_num} and {highest_num}")
+
+
+
+
+
+
+
+# ### Rock Paper Scissors game
+# import random
+
+# options = ("rock", "paper", "scissors")
+# #player = None   #we create a variable name player to store the player's choice
+# #computer = random.choice(options)
+# running = True  #This is to set rock paper scissors game to be able to play again if user want
+
+
+# while running:   #this is equal to while running == True:  and we dont set while True: because it's hard to find where the break statement is
+    
+#     player = None   #we move player and computer variable down here (inside while loop) because every time we start a new game,
+#     computer = random.choice(options) #we will reset the player as well as the computer, so we put inside the while loop.
+
+#     # we need condition to limit user to only put input of rock, paper, scissors choices
+#     while player not in options:  # this while loop = if the player doesn't pick one of these options, this while loop will continue forever
+#         player = input("Enter a choice (rock, paper,scissors): ") #once we pick something within our options, we then escape while loop
+
+#     print(f"Player: {player}")
+#     print(f"Computer: {computer}")
+
+#     if player == computer:
+#         print("It's a tie!")
+#     elif player == "rock" and computer == "scissors":
+#         print("You win!")
+#     elif player == "paper" and computer == "rock":
+#         print("You win!")
+#     elif player == "scissors" and computer == "paper":
+#         print("You win!")
+#     else:
+#         print("You lose!")
+    
+#     if not input("Play agian? (y/n): ").lower() == "y":  #this line = if the user input, after making it lowercase, doesn't equal to y 
+#         running = False                                  #for yes, it will set running to be False (aka stop running game again)
+
+# print("Thanks for playing!")
+
+
+
+
+
+
+# ###dice roller program in python
+# import random
+
+# #we need these symbol from these unic code characters to build ASI art some dices
+# # print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518")
+# #● ┌ ─ ┐ │ └ ┘
+
+# #basic box shape
+# "┌─────────┐"
+# "│         │"
+# "│         │"
+# "│         │"
+# "└─────────┘"
+
+# ##let's create a dictionary aka. key:value pair!!!
+# dice_art = {
+#     1: ("┌─────────┐",
+#         "│         │",
+#         "│    ●    │",
+#         "│         │",
+#         "└─────────┘"),
+#     2: ("┌─────────┐",
+#         "│  ●      │",
+#         "│         │",
+#         "│      ●  │",
+#         "└─────────┘"),
+#     3: ("┌─────────┐",
+#         "│  ●      │",
+#         "│    ●    │",
+#         "│      ●  │",
+#         "└─────────┘"),
+#     4: ("┌─────────┐",
+#         "│  ●   ●  │",
+#         "│         │",
+#         "│  ●   ●  │",
+#         "└─────────┘"),
+#     5: ("┌─────────┐",
+#         "│  ●   ●  │",
+#         "│    ●    │",
+#         "│  ●   ●  │",
+#         "└─────────┘"),
+#     6: ("┌─────────┐",
+#         "│  ●   ●  │",
+#         "│  ●   ●  │",
+#         "│  ●   ●  │",
+#         "└─────────┘")
+# } 
+
+# dice =[]  #dice will be numbers, randomly generated between 1 and 6
+# total = 0
+# num_of_dice = int(input("How many dice?: "))
+
+# for die in range(num_of_dice):
+#     dice.append(random.randint(1, 6)) #We need dice.append to store each roll in the list named dice = []; otherwise only the last roll would remain.
+
+# # for die in range(num_of_dice):
+# #     for line in dice_art.get(dice[die]): #we getting value in our dictionary at the given key.
+# #         print(line)
+
+# ##to make all the dices in the horizon line display
+# for line in range(5):
+#     for die in dice:
+#         print(dice_art.get(die)[line], end="")
+#     print()
+
+# for die in dice:
+#     total += die
+# print(f"total: {total}")
+
+
+
+
+
+
+
+# function = A block of reuseable code
+#            place () after the function name to invokee it
+
+# #example: sing Happy Birth Day 3 times ปกติต้องทำยาวๆ 3รอบ
+# print("Happy birthday to you!")
+# print("You are old!")
+# print("Happy birthday to you!")
+# print()
+
+# print("Happy birthday to you!")
+# print("You are old!")
+# print("Happy birthday to you!")
+# print()
+
+# print("Happy birthday to you!")
+# print("You are old!")
+# print("Happy birthday to you!")
+# print()
+
+## there is better way that doesn't involve repeating our code or using loops
+### By writing this code once, then reuse it whenever I need to
+### That's where functions come in fuctions => "def" plus fuction name and ():
+# def happy_birthday():
+#     print("Happy birthday to you!")
+#     print("You are old!")
+#     print("Happy birthday to you!")
+#     print()
+
+# #to invoke the function you type function name then add a parenthesis ()
+# happy_birthday()
+# happy_birthday()
+# happy_birthday()
+
+# #using argument inside the parenthesis aka("argument"), you can send values or variables directly to a function
+# #BUT you need a matching set of parameters that are in order for the argument,
+# def happy_birthday(name, age):             # and you can have more than 1 argument ex. name, age (the order does matter!!)
+#     print(f"Happy birthday to {name}!")    #ใช้เหมือนการแทนตัวแปร with using f string and {variable/argument}
+#     print(f"You are {age} years old!")
+#     print("Happy birthday to you!")
+#     print()
+
+# #to invoke the function you type function name then add a parenthesis ()
+# happy_birthday("Bro", 20)
+# happy_birthday("Steve", 30)
+# happy_birthday("Joe", 40)
+
+
+##this is called the positional argument
+# def happy_birthday(x, y):             # the order does matter!!
+#     print(f"Happy birthday to {x}!")    #ใช้เหมือนการแทนตัวแปร x,y with using f string and {variable/argument}
+#     print(f"You are {y} years old!")
+#     print("Happy birthday to you!")
+#     print()
+
+# #to invoke the function you type function name then add a parenthesis ()
+# happy_birthday("Bro", 20)
+# happy_birthday("Steve", 30)
+# happy_birthday("Joe", 40)
+
+# def display_invoice(username, amount, due_date):   # the order does matter!!
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("Joe", 100.25, "01/02")
+
+###explain return statement
+# return = statement used to end a function
+#          and send a result back to the caller
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x, y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+# def create_name(first, last):  #function
+#     first = first.capitalize()
+#     last = last.capitalize()
+#     return first + " " + last   #using return statement, you can return data back to the function
+
+# full_name = create_name("spongebob", "squarepants")
+
+# print(full_name)     
+# #function is a section of reusable code. To use function, you type function's name, add a set of parenthesis
+# #you can send some data (aka arguments) while needed matching set of parameters, you also do have option of returning data back to place in which you invoke function
+
+
+
+
+
+
+
+
+
+
+
+
+# # default arguments = A default value for certain parameters default is used when that argument is omitted
+# #                     make your functions more flexible, reduce # of arguments 1.positional, 2.DEFAULT, 3.keyword, 4.arbitrary
+
+# #last topic we already dicussed positional argument, 
+# #so today we gonna discuss DEFAULT argument!!!
+
+# #Let's define function to calculate the net price, it will be 3 parameters list_price, discount, tax
+# def net_price(list_price, discount, tax):
+#     return list_price * (1 - discount) * (1 + tax)
+
+# print(net_price(500, 0, 0.05))
+#example, most of the time discount rate and tax rate is always the same, what we could do to make function more flexible
+
+# #we can do it by set these two (discount, tax) parameters to have a DEFAULT value, like this below
+# def net_price(list_price, discount=0, tax=0.05):    #we set discount and tax as default value
+#     return list_price * (1 - discount) * (1 + tax)
+
+# # print(net_price(500))
+# # print(net_price(500 ,0.1))  # this is the case when if there is someone who has a discount
+#                               # if we're passing in an argument for discount, we will use the discount that passed in rather than the default
+# # if user not pay tax too 
+# # print(net_price(500 ,0.1 ,0)) 
+
+# # EXERCISE, We will create a count up timer
+# import time  #import time module
+
+# def count(start, end):
+#     for x in range(start, end+1): #for loop # within the range function the second argument is exclusive, so we need to add one (+1) 
+#         print(x)
+#         time.sleep(1) #to make this thread that's running the program sleep
+#     print("DONE!")
+
+# count(0, 10)  #to invoke the function
+
+# #let's assume most of the time user want to start the time at 0
+# #so we gonna set 0 as default
+# import time  #import time module
+
+# def count(end, start=0):  #but remember if you use any default argument, you want to be sure that they're after any positional arguments
+#     for x in range(start, end+1): 
+#         print(x)
+#         time.sleep(1) #to make this thread that's running the program sleep
+#     print("DONE!")
+
+# count(10)  #to invoke the function
+ 
+# import time  #import time module
+
+# def count(end, start=0):  #but remember if you use any default argument, you want to be sure that they're after any positional arguments
+#     for x in range(start, end+1): 
+#         print(x)
+#         time.sleep(1) #to make this thread that's running the program sleep
+#     print("DONE!")
+
+# count(30, 15)  #to invoke the function
+##so in conclusion DEFAULT argument is used when an argument are consistent most of the time
+
+
+
+
+
+
+
+# #KEYWORD arguments = an argument preceded by an identifier helps with readability order of arguments doesn't matter
+# #                      1. positional 2. default 3. KEYWORD 4. arbitrary
+
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title}{first} {last}")
+
+# hello("Hello", "Mr.", "Jayson", "Tatum")  ##right now we currently using positional argument(the order matter)
+
+# #if we want to change position we can use keyword method for example hello("Hello", "Jayson", "Tatum", "Mr.") order มันผิดต้องแก้เป็น
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title}{first} {last}")
+
+# hello(first = "Jayson", last = "Tatum", title = "Mr.", greeting ="Hello")  #this is the keyword argument aka. order doesn't matter
+# ##and also, if there is positional argument inside the (), make sure any positional argument are first
+
+# #more example, print number 1 to 10 using for loop
+# for x in range(1,11): #we use 11 as we want 1 to 10 because the second argument is exclusive
+#     print(x, end=" ") #end=" "
+
+# #create function to generate a phone number, but we need to pass in a country code, area code, first fews digits, last fews digits
+# def get_phone(country, area, first, last):
+#     return f"{country}-{area}-{first}-{last}"
+
+# phone_num = get_phone(country=1, area=123, first=456, last=7890)
+
+# print()
+# print(phone_num)
+
+
+# #create function to generate a phone number, but we need to pass in a country code, area code, first fews digits, last fews digits
+# def get_phone(country, area, first, last):
+#     return f"{country}-{area}-{first}-{last}"
+
+# phone_num = get_phone(country=0, area=81, first=171, last=9958)
+
+# print()
+# print(phone_num)
+
+
+
+
+
+
+
+# # *args    = allows you to pass multiple non-key arguments
+# # **kwargs = allows you to pass multiple keyword-arguments
+# #            * unpacking operator
+# #                      1. positional 2. default 3. keyword 4. ARBITRARY
+# #ARBITRARY meaning a varying amount of arguments, as we dont know how many arguments the user is gonna pass in when they invoke function
+
+
+# def add(a, b):
+#     return a + b
+
+# # print(add(1, 2))
+#                         #what if we want to add 3 arguments this time
+#                         # print(add(1, 2, 3)) #this cannot be worked
+
+# #so we need to modify the function, so it can accept a varying amount of arguments in any amount
+# def add(*args):    # we replace the parameters with the *args (aka arguments, *args = allows you to pass multiple non-key arguments)
+#     print(type(args)) #when we use the unpacking operator, so now the argument will be packed all into a tupple
+#     #we get <class 'tuple'>
+# add(1, 2, 3)
+
+# #tips: *args we can change name *args to *nums, because the name of the parameter isn't as important as the unpacking(* ดอกจัน)
+# #*args → packs all inputs into a tuple
+# def add(*args):    # we replace the parameters with the *args (aka arguments, *args = allows you to pass multiple non-key arguments)
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+# print(add(1, 2, 3, 4, 5)) #now we get result of 15
+
+# #function to display somebody name
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end=" ")
+
+# display_name("Dr.", "Jayson", "Leon", "Tatum", "III")
+# #you get the result as Dr. Jayson Leon Tatum III, because with the unpacking operator (* and args) we can pack all the arguments ชื่อทั้งหมด into a tupple
+
+# #Now Let's discuss **kwargs = allows you to pass multiple keyword-arguments
+# #**kwargs is keyword-arguments
+# def print_address(**kwargs):
+#     print(type(kwargs)) 
+#     #we get <class 'dict'>
+#     #so its mean we can treat kwargs as it is a dictionary
+
+# print()
+# def print_address(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
+
+# print_address(street="123 Newtown St.", 
+#               city="BKK", 
+#               state="Rama3", 
+#               zip="10101")
+
+
+# #now we will have an excercise with both *args and **kwargs together
+# #we will print shipping label
+# def shipping_label(*args, **kwargs):
+#     for arg in args:
+#         print(arg, end=" ")
+#     print()
+#     for value in kwargs.values():
+#         print(value, end="")
+
+#                                     # this def shipping_label(*args, **kwargs): function is designed to accept both *args and **kwargs
+# shipping_label("Dr.", "Fernando", "Torres", "III",    #this frist line is a arbitrary positional arguments
+#                street="123 St mary.",                 #and the rest are arbitrary keyword arguments
+#                city=" BKK",                            #
+#                apt=" 100",                             #
+#                state=" Duke",                          #
+#                zip=" 10101")                           #
+
+
+
+
+
+
+
+
+
+# #Iteralbes = An object/collection that can return its elements one at a time,   ######return one at a time using a loop!!!!!
+# #            allowing it to be iterated over in a loop
+
+# #Iterables is a category of returning its element one at a time
+
+# numbers = [1, 2, 3, 4, 5]  #List [], are considered iterable, we can use them within the for loop.
+
+# for item in reversed(numbers):  #reversed() to iterate reverse or backwards
+#     print(item, end=" ")  #In short: end=" " tells "print" to finish with a space instead of starting a new line.
+#                           # if we change to end="-" it will tells "print" to finish with a - instead of starting a new line
+
+# print()
+# numbers = (1, 2, 3, 4, 5)  # tupple () also iteratable
+
+# for item in numbers: 
+#     print(item)
+
+# print()
+# #Let's cover set{}
+# fruits = {"apple", "orange", "banana", "coconut"} #set{} cannot be reverse
+
+# for fruit in fruits:
+#     print(fruit)
+
+# #Let's cover string (aka character)
+# name = "Jayson Tatum"
+
+# for character in name:
+#     print(character, end="")
+
+# print()
+# #lastly, dictionary is use set, and within the set{} there is a {key:value} pair(s) 
+# my_dictionary = {"A": 1, "B": 2, "C": 3}
+# #if you iterate over a dictionary, the dictionary gonna return all the keys but not the value
+# for key in my_dictionary:
+#     print(key)
+
+# #if you need the values, we gonna follow this iterable of my dictionary, the .values() method!!
+# my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+# for value in my_dictionary.values():
+#     print(value)
+
+# #if you need both keys and values, you gonna use .items() method!!
+# my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+# for key, value in my_dictionary.items():
+#     print(key, value)
+
+# #we can reformat the output however we want. use the f"string"
+# my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+# for key, value in my_dictionary.items():
+#     print(f"{key} = {value}")
+
+
+
+
+
+
+
+
+
+
+
+# Membership operators = used to test whether a value or variable is found in a sequence (string, list, tuple, set, or dictionary)
+#                       1. in
+#                       2. not in 
+
+#we want to see if my letter is found in my word. (aka find in a string of a word)
+word = "APPLE"
+
+letter = input("Guess a letter in the secret word: ")
+#The in membership operator will test to see if a value or a variable is found within a sequence.
+if letter in word:  #"in" is going to return a boolean value of TRUE if that letter is found or FALSE if it's not
+    print(f"There is a {letter}")
+else:
+    print(f"{letter} was not found")
+
+
+#for the inverse of the in membership operator (aka not in)
+#if the value or variable is not found in this sequence, it return TRUE, otherwise FALSE.
+if letter not in word:  #if now it's "not in" we have to swicht/flip the statements/ที่เราพิม around, so it'll deliver same result as we want
+    print(f"{letter} was not found")
+else:
+    print(f"There is a {letter}")
+
+#now let's searching for a value or variable found within a set (similar to list, tuple)
+#set of student
+students = {"Jayson", "Fernando", "Luis"}
+
+student = input("Enter the name of a student: ")
+
+if student in students:
+    print(f"{student} is a student")
+else:
+    print(f"{student} was not found")
+
+#we can do the opposite of in (aka not in)
+if student not in students:
+    print(f"{student} was not found")
+else:
+    print(f"{student} is a student")
+
+#now we gonna cover the dictionary
+grades = {}
